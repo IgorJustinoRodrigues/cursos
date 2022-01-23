1a2ca42df00b77c7ddea77a8c5ca2631cb75b5ec
David-codificador <natan_seabra@hotmail.com>
2022-01-23 11:05:47 -0300

    Update verCursos.blade.php

--- resources/views/painelAluno/aula/verCursos.blade.php
+++ resources/views/painelAluno/aula/verCursos.blade.php
@@ -13,45 +13,42 @@
             <div class="media-body">
                 <h1 class="h2">Meus cursos</h1>
             </div>
-            <div class="media-right">
-                <div class="btn-group btn-group-sm">
-                    <a href="#" class="btn btn-white active"><i class="material-icons">list</i></a>
-                    <a href="#" class="btn btn-white"><i class="material-icons">dashboard</i></a>
-                </div>
-            </div>
         </div>
-        <div class="card-columns">
-            <div class="card">
-                <div class="card-header">
-                    <div class="media">
-                        <div class="media-left">
-                            <a href="">
-                                <img src="{{ URL::asset('template/images/vuejs.png') }}" alt="Card image cap" width="100"
-                                    class="rounded">
-                            </a>
-                        </div>
-                        <div class="media-body">
-                            <h4 class="card-title m-0"><a href="">Learn VueJs the easy way!</a></h4>
-                            <small class="text-muted">Lessons: 3 of 7</small>
+
+        @foreach ($cursos as $linha)
+            <div class="card-columns">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="media">
+                            <div class="media-left">
+                                <a href="">
+
+                                    @if ($linha->imagem != '')
+                                        <img src="{{ URL::asset('storage/' . $linha->imagem) }}" alt="" width="100"
+                                            class="rounded">
+                                    @else
+                                        <img src="{{ URL::asset('storage/imagemCurso/padrao.png') }}" alt="" width="100"
+                                            class="rounded">
+                                    @endif
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="card-title m-0"><a href="">{{ $linha->nome }}</a></h4>
+                                <small class="text-muted">Aulas: 3 de 7</small>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="progress rounded-0">
-                    <div class="progress-bar progress-bar-striped bg-primary" role="progressbar" style="width: 40%"
-                        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <div class="card-footer bg-white">
-                    <a href="" class="btn btn-primary btn-sm">Continue <i
-                            class="material-icons btn__icon--right">play_circle_outline</i></a>
+                    <div class="progress rounded-0">
+                        <div class="progress-bar progress-bar-striped bg-primary" role="progressbar" style="width: 10%"
+                            aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <div class="card-footer bg-white">
+                        <a href="" class="btn btn-primary btn-sm">Acessar Aulas <i
+                                class="material-icons btn__icon--right">play_circle_outline</i></a>
+                    </div>
                 </div>
             </div>
-
-           
-
-
-
-    
-        </div>
+        @endforeach
     </div>
 
 
