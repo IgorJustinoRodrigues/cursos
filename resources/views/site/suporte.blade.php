46f37a4c4d2fa0fd2c3b8088ba352ad8c94487d0
Igor Justino Rodrigues <igor98rodrigues@gmail.com>
2022-02-04 14:53:46 -0300

    Update suporte.blade.php

--- resources/views/site/suporte.blade.php
+++ resources/views/site/suporte.blade.php
@@ -28,8 +28,10 @@
             </div>
             <div class="section-wrapper">
                 <div class="row g-4 row-cols-lg-3 row-cols-sm-2 row-cols-1 justify-content-center">
+
+
                     <div class="col">
-                        <div class="feature-item">
+                        <div class="feature-item widget widget-category">
                             <div class="feature-inner">
                                 <div class="feature-thumb">
                                     <i class="icofont-learn" style="font-size: 50px;"></i>
@@ -41,83 +43,34 @@
                                     <a href="#" class="lab-btn-text">Conhecer <span></span></a>
                                 </div>
                             </div>
+                            <ul class="widget-wrapper">
+                                <li>
+                                    <a href="#" class="d-flex flex-wrap justify-content-between"><span><i class="icofont-double-right"></i>Themeforest</span><span>06</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex flex-wrap justify-content-between"><span><i class="icofont-double-right"></i>Photodune</span><span>11</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex active flex-wrap justify-content-between"><span><i class="icofont-double-right"></i>Codecanyon</span><span>07</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex flex-wrap justify-content-between"><span><i class="icofont-double-right"></i>GRaphicdriver</span><span>09</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex flex-wrap justify-content-between"><span><i class="icofont-double-right"></i>Wordpress</span><span>50</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex flex-wrap justify-content-between"><span><i class="icofont-double-right"></i>Joomla</span><span>20</span></a>
+                                </li>
+                                <li>
+                                    <a href="#" class="d-flex flex-wrap justify-content-between"><span><i class="icofont-double-right"></i>3docean</span><span>93</span></a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="feature-item">
-                            <div class="feature-inner">
-                                <div class="feature-thumb">
-                                    <i class="icofont-certificate" style="font-size: 50px;"></i>
-                                </div>
-                                <div class="feature-content">
-                                    <a href="#">
-                                        <h5>Certificados</h5>
-                                    </a>
-                                    <a href="#" class="lab-btn-text">Conhecer <span></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="feature-item">
-                            <div class="feature-inner">
-                                <div class="feature-thumb">
-                                    <i class="icofont-graduate-alt" style="font-size: 50px;"></i>
-                                </div>
-                                <div class="feature-content">
-                                    <a href="#">
-                                        <h5>Suporte Aluno</h5>
-                                    </a>
-                                    <a href="#" class="lab-btn-text">Conhecer <span></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="feature-item">
-                            <div class="feature-inner">
-                                <div class="feature-thumb">
-                                    <i class="icofont-business-man-alt-3" style="font-size: 50px;"></i>
-                                </div>
-                                <div class="feature-content">
-                                    <a href="#">
-                                        <h5>Suporte Vendedor</h5>
-                                    </a>
-                                    <a href="#" class="lab-btn-text">Conhecer <span></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="feature-item">
-                            <div class="feature-inner">
-                                <div class="feature-thumb">
-                                    <i class="icofont-business-man-alt-1" style="font-size: 50px;"></i>
-                                </div>
-                                <div class="feature-content">
-                                    <a href="#">
-                                        <h5>Suporte Unidade</h5>
-                                    </a>
-                                    <a href="#" class="lab-btn-text">Conhecer <span></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="feature-item">
-                            <div class="feature-inner">
-                                <div class="feature-thumb">
-                                    <i class="icofont-man-in-glasses" style="font-size: 50px;"></i>
-                                </div>
-                                <div class="feature-content">
-                                    <a href="#">
-                                        <h5>Suporte Parceiro</h5>
-                                    </a>
-                                    <a href="#" class="lab-btn-text">Conhecer <span></span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+
+
                 </div>
             </div>
         </div>
