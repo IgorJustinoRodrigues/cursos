3e6c16d5f47302d16d6ea9b12b86b2a9bdea4974
David-codificador <natan_seabra@hotmail.com>
2022-02-07 16:42:53 -0300

    Correções
    Toda tela que tem o campo estado no painel admin está dentro de um select

--- resources/views/painelAdmin/aluno/cadastro.blade.php
+++ resources/views/painelAdmin/aluno/cadastro.blade.php
@@ -144,10 +144,41 @@
                             </div>
 
                             <div class="col-12 col-md-4 mb-3">
-                                <label class="form-label" for="estado">Estado</label>
-                                <input type="text" class="form-control" id="estado" name="estado" placeholder="Estado"
-                                    value="{{ old('estado') }}">
+                                <label class="form-label" for="cidade">Estado</label>
+                                <div class="col-md-12">
+                                    <select id="estado" name="estado" class="form-control">
+                                        <option value="" @if (old('estado') == '') selected @endif>-</option>
+                                        <option value="AC" @if (old('estado') == 'AC') selected @endif>Acre</option>
+                                        <option value="AL" @if (old('estado') == 'AL') selected @endif>Alagoas</option>
+                                        <option value="AP" @if (old('estado') == 'AP') selected @endif>Amapá</option>
+                                        <option value="AM" @if (old('estado') == 'AM') selected @endif>Amazonas</option>
+                                        <option value="BA" @if (old('estado') == 'BA') selected @endif>Bahia</option>
+                                        <option value="CE" @if (old('estado') == 'CE') selected @endif>Ceará</option>
+                                        <option value="DF" @if (old('estado') == 'DF') selected @endif>Distrito Federal</option>
+                                        <option value="ES" @if (old('estado') == 'ES') selected @endif>Espírito Santo</option>
+                                        <option value="GO" @if (old('estado') == 'GO') selected @endif>Goiás</option>
+                                        <option value="MA" @if (old('estado') == 'MA') selected @endif>Maranhão</option>
+                                        <option value="MT" @if (old('estado') == 'MT') selected @endif>Mato Grosso</option>
+                                        <option value="MS" @if (old('estado') == 'MS') selected @endif>Mato Grosso do Sul</option>
+                                        <option value="MG" @if (old('estado') == 'MG') selected @endif>Minas Gerais</option>
+                                        <option value="PA" @if (old('estado') == 'PA') selected @endif>Pará</option>
+                                        <option value="PB" @if (old('estado') == 'PB') selected @endif>Paraíba</option>
+                                        <option value="PR" @if (old('estado') == 'PR') selected @endif>Paraná</option>
+                                        <option value="PE" @if (old('estado') == 'PE') selected @endif>Pernambuco</option>
+                                        <option value="PI" @if (old('estado') == 'PI') selected @endif>Piauí</option>
+                                        <option value="RJ" @if (old('estado') == 'RJ') selected @endif>Rio de Janeiro</option>
+                                        <option value="RN" @if (old('estado') == 'RN') selected @endif>Rio Grande do Norte</option>
+                                        <option value="RS" @if (old('estado') == 'RS') selected @endif>Rio Grande do Sul</option>
+                                        <option value="RO" @if (old('estado') == 'RO') selected @endif>Rondônia</option>
+                                        <option value="RR" @if (old('estado') == 'RR') selected @endif>Roraima</option>
+                                        <option value="SC" @if (old('estado') == 'SC') selected @endif>Santa Catarina</option>
+                                        <option value="SP" @if (old('estado') == 'SP') selected @endif>São Paulo</option>
+                                        <option value="SE" @if (old('estado') == 'SE') selected @endif>Sergipe</option>
+                                        <option value="TO" @if (old('estado') == 'TO') selected @endif>Tocantins</option>
+                                    </select>
+                                </div>
                             </div>
+
 
                             <div class="col-12 col-md-4 mb-3">
                                 <label class="form-label" for="status">Status</label>
